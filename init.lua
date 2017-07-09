9e301b9da4a66e867235a20b8fd37993e2444c3c
AntumDeluge <antumdeluge@gmail.com>
2017-07-08 22:04:41 -0700

    Replace deprecated function 'intllib.Getter':
    - Check first for 'intllib.make_gettext_pair', otherwise continue using
    odl function.

--- init.lua
+++ init.lua
@@ -38,7 +38,13 @@ local creative_mode = minetest.settings:get_bool("creative_mode")
 -- Boilerplate to support localized strings if intllib mod is installed.
 local S
 if minetest.global_exists("intllib") then
-	S = intllib.Getter()
+	if intllib.make_gettext_pair then
+		-- New method using gettext.
+		S = intllib.make_gettext_pair()
+	else
+		-- Old method using text files.
+		S = intllib.Getter()
+	end
 else
 	S = function(s) return s end
 end
